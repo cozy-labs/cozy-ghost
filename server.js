33d49022506e469945e0ab676e78e145a71341b9
Cyril BAREME <cyril.bareme@gmail.com>
2015-03-18 17:20:16 +0100

    try without https

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ cozydb.configure(__dirname, null, function(){
     }
 
     console.log("domain : ", domain);
-    domain = "https://" + domain;
+    domain = "http://" + domain;
     // ghostConfig.production.url = require('url').resolve(domain, '/public/blog/');
     ghostConfig.development.url = require('url').resolve(domain, '/public/blog/');
 
