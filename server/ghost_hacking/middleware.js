86d5f00491fcd9edb7e429f1901965a6e332dc22
Cyril BAREME <cyril.bareme@gmail.com>
2015-03-18 09:46:31 +0100

    add debug trace

--- server/ghost_hacking/middleware.js
+++ server/ghost_hacking/middleware.js
@@ -2,12 +2,13 @@ var path = require('path');
 
 module.exports = function(expressApp) {
     expressApp.all('*', function(req, res, next){
+      console.log("orig url : ", req.url);
       if(req.url.match(/^\/public.*/)){
         req.url = req.url.substring("public/".length);
-        // console.log("new url : ", req.url);
+        console.log("new url : ", req.url);
       }else{
         req.url = path.join("/ghost/", req.url);
-        // console.log("private url", req.url);
+        console.log("private url", req.url);
       }
       next();
     });
